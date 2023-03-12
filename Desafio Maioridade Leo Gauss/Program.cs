4f6fabc5bc06996d798fa30b2a76c69cb174d6be
Pedro Cardoso <pedro.contato24@gmail.com>
2023-03-12 17:08:52 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio Maioridade Leo Gauss/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Maioridade_Leo_Gauss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.WriteLine("Digite sua idade");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você é maior de idade!");
+            }
+            else
+            {
+                Console.WriteLine("Você não é maior de idade." + " Ainda faltam " + (18 - idade) + " anos para atingir a maioridade penal.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
